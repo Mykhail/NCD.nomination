eef9d406d39844196ae66953407b175be38170ab
Mikhail Sobko <mikhail.sobko@gmail.com>
2022-05-25 18:47:36 +0300

    project preparation

--- scripts/1.recruitement-deploy.sh
+++ scripts/1.recruitement-deploy.sh
@@ -20,28 +20,18 @@ echo "(edit scripts/1.dev-deploy.sh to deploy other contract)"
 echo ---------------------------------------------------------
 echo
 
-# uncomment out the line below to deploy the other example contract
-# near dev-deploy ./build/debug/simple.wasm
-
-# comment the line below to deploy the other example contract
-# near dev-deploy ./build/debug/singleton.wasm
-near deploy cert.somix11.testnet ./build/debug/singleton.wasm
+near deploy $CONTRACT ./build/debug/recruitment.wasm
 
 echo
 echo
 echo ---------------------------------------------------------
 echo "Step 3: Prepare your environment for next steps"
 echo
-echo "(a) find the contract (account) name in the message above"
-echo "    it will look like this: [ Account id: dev-###-### ]"
-echo
-echo "(b) set an environment variable using this account name"
+echo "(a) set an environment variable using target account name"
 echo "    see example below (this may not work on Windows)"
 echo
 echo ---------------------------------------------------------
 echo 'export CONTRACT=<dev-123-456>'
-# uncomment this line for a useful hint when using the singleton style
-# echo "near call \$CONTRACT init --accountId \$CONTRACT"
 echo ---------------------------------------------------------
 echo
 
